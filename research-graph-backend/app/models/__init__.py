c87c8cb0183d560ea9f41ed8a72dc09b56a52a0c
Atif <atif.hyd123@gmail.com>
2025-10-02 19:09:10 +0530

    models package initialization

---
+++ research-graph-backend/app/models/__init__.py
@@ -0,0 +1,10 @@
+from app.models.paper import Paper, paper_authors
+from app.models.author import Author
+from app.models.citation import Citation
+
+__all__ = [
+  'Paper',
+  'Author',
+  'Citation',
+  'paper_authors'
+]
