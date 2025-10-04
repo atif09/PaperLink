ae3aeb0970006ab74455a12042e343bd3bbf50a3
Atif <atif.hyd123@gmail.com>
2025-10-04 16:25:38 +0530

    exporting library blueprint

--- research-graph-backend/app/routes/__init__.py
+++ research-graph-backend/app/routes/__init__.py
@@ -1,5 +1,6 @@
 from app.routes.search import search_bp
 from app.routes.papers import papers_bp
+from app.routes.library import library_bp
 
-__all__ = ['search_bp', 'papers_bp']
+__all__ = ['search_bp', 'papers_bp', 'library_bp']
 
